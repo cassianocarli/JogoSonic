b72f1de100c7c89ae07ada272bd6fe672edc1b7c
cassianocarli <138841588+cassianocarli@users.noreply.github.com>
2023-12-01 09:39:16 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+const sonic = document.querySelector(".sonic");
+const eggman = document.querySelector(".eggman");
+const fundo = document.querySelector(".fundo");
+
+const pulo = () => {
+  sonic.classList.add("pulo");
+  sonic.src = "./pasta/Sonic-pulando.gif";
+
+  setTimeout(() => {
+    sonic.classList.remove("pulo");
+    sonic.src = "./pasta/Sonic.gif";
+  }, 900);
+};
+
+const loop = setInterval(() => {
+  const eggmanPosition = eggman.offsetLeft;
+  const sonicPosition = +window
+    .getComputedStyle(sonic)
+    .bottom.replace("px", "");
+
+  if (eggmanPosition < 190 && eggmanPosition > 0 && sonicPosition < 300) {
+    eggman.style.animation = "none";
+    eggman.style.left = `${eggmanPosition}px`;
+
+    sonic.style.animation = "none";
+    sonic.src = "./pasta/Sonic-Loss.gif";
+    sonic.style.width = "400px";
+
+    fundo.src = "./pasta/Gameover.png";
+  }
+}, 10);
+
+document.addEventListener("click", pulo);
